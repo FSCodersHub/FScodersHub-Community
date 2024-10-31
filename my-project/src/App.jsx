e3dd99b79528ef0e5979214b63af6a8563dd7351
AYMANE RACHID <aymanerachid47@gmail.com>
2024-10-31 09:16:05 +0000

    add contactSection.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import ContactSection from './components/ContactSection'
 function App() {
   return (
    <>
-   <button className='btn btn-info'>
-       good luck team!
-   </button>
+ 
+  
    </>
   )
 }
