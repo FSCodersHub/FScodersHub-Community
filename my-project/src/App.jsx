9c3ecbba7f2f89df804d931d9fdf7e57d0569dfe
aymane <aymanerachid47@gmail.com>
2024-11-24 01:38:45 +0100

    integration des composants

---
+++ my-project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ContactSection from './components/ContactSection'
+import HomeSection from './components/HomeSection'
+import ProjectSection from './components/ProjectSection'
+import QuestionsSection from './components/QuestionSection'
+
+import Nav from './components/nav'
+import Footer from './components/Footer'
+import ServicesSection from './components/ServicesSection'
+function App() {
+  return (
+    <>
+      <Nav/>
+      <HomeSection />
+      <ProjectSection/>
+      <ServicesSection/>
+      <ContactSection/> 
+      <QuestionsSection/>
+      <Footer/>
+ 
+    </>
+  )
+}
+export default App
